146054d2b871e6dbc304e4cdc41b19ba31d4dbaa
SeagullinSeagulls <42948046+SeagullisLearningToCode@users.noreply.github.com>
2021-01-14 14:09:47 -0500

    Create Japanese.py

---
+++ Japanese.py
@@ -0,0 +1,19 @@
+# Japanese (Google Translated)
+from datetime import *
+td = datetime.now()
+
+# Strings
+yt_friendly = "何もない"
+hp_txt = "健康"
+air_txt = "O2"
+tit_bttm_txt = "PCリメイクのコンセプト"
+if yt_friendly is False:
+    wtrmark_txt = "SIS Programming (2021)"
+elif yt_friendly == "何もない":
+    wtrmark_txt = f"/--_ ベータ: {td}"
+else:
+    wtrmark_txt = "コード化：SeagullInSeagulls / --_"
+
+j_ws_str1 = "警告"
+j_ws_str2 = "違法であろうとなかろうと、餌を与えたり、餌を与えようとしたり、野生生物に嫌がらせをしたりすることは有害です。"
+j_ws_str3 = "それは有害であるだけでなく、あなたは彼らのために作られていない食べ物に頼るように彼らの食事を変えています"
