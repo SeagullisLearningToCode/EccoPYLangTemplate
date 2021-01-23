c863a21921e433dc42a4a8cb3fb124df82dc0a87
SeagullinSeagulls <42948046+SeagullisLearningToCode@users.noreply.github.com>
2021-01-22 21:15:14 -0500

    Update English.py

--- English.py
+++ English.py
@@ -1,7 +1,10 @@
 from datetime import *
+# Today's Date
 td = date.today() # More simple than "datetime.now()"
 td_c_s = str(td) # Converts the current date into a String
 td_c_s_yo = td_c_s[0:4] # Get year only
+# Today's Time
+tt = datetime.now()
 
 # Strings --> Ingame and HUD
 yt_friendly = "Nothing"
@@ -15,6 +18,8 @@ elif yt_friendly == "Nothing":
 else:
     wtrmark_txt = "Coded by: SeagullInSeagulls /--_"
 # Warning Screen
-hu_ws_str1 = "WARNING"
-hu_ws_str2 = "Even if it's illegal or not, it's harmful to feed or attempt to feed and even harass Wildlife"
-hu_ws_str3 = "Not only it's harmful but you are creating unhealthy diets for the wildlife."
+e_ws_str1 = "WARNING"
+e_ws_str2 = "Even if it's illegal or not, it's harmful to feed or attempt to feed and even harass Wildlife"
+e_ws_str3 = "Not only it's harmful but you are creating unhealthy diets for the wildlife."
+e_ws_str4 = f"DATE: {td.strftime('%Y.%m.%d')}"
+e_ws_str5 = f"{tt.strftime('%p %I:%M')}"
